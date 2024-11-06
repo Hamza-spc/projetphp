563657d0a103eb249ee78a7bf075751ded405749
Hamza <hamza.2simou@gmail.com>
2024-11-06 22:40:44 +0100

    login.php

--- login.php
+++ login.php
@@ -1,6 +1,5 @@
 <?php
-session_start();
-include('validate.php'); // Include validation file
+include('validate.php');
 if (!isset($_POST['submit'])) {
     echo "<p style='color: red;'>Please log in.</p>";
 }
